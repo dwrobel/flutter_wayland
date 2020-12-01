ef9b8c86c33247bd149b1c436147e2674f987c01
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-12-01 19:59:35 +0100

    Add presentation-time implementation

--- src/wayland_display.cc
+++ src/wayland_display.cc
@@ -378,13 +378,28 @@ const struct wp_presentation_feedback_listener WaylandDisplay::kPresentationFeed
     .sync_output = [](void *data, struct wp_presentation_feedback *wp_presentation_feedback, struct wl_output *output) {},
     .presented =
         [](void *data, struct wp_presentation_feedback *wp_presentation_feedback, uint32_t tv_sec_hi, uint32_t tv_sec_lo, uint32_t tv_nsec, uint32_t refresh, uint32_t seq_hi, uint32_t seq_lo, uint32_t flags) {
+          const auto t0            = FlutterEngineGetCurrentTime();
           WaylandDisplay *const wd = get_wayland_display(data);
-          wp_presentation_feedback_add_listener(::wp_presentation_feedback(wd->presentation_, wd->surface_), &kPresentationFeedbackListener, data);
+
+          const uint64_t new_last_frame_ns = (((static_cast<uint64_t>(tv_sec_hi) << 32) + tv_sec_lo) * 1000000000) + tv_nsec;
+
+          if (refresh != wd->vblank_time_ns_) {
+            static auto displayed = false;
+
+            if (!displayed) {
+              printf("WARN: Variable display rate output: vblank_time_ns: %ju refresh: %u\n", wd->vblank_time_ns_, refresh);
+              displayed = true;
+            }
+          }
+
+          printf("[%ju] vsync.dpts %ju %-9jd %.3f \n", t0, static_cast<uintmax_t>(new_last_frame_ns), static_cast<intmax_t>(t0 > new_last_frame_ns ? t0 - new_last_frame_ns : new_last_frame_ns - t0), (new_last_frame_ns - wd->last_frame_) / 1e9);
+
+          wd->last_frame_ = new_last_frame_ns;
         },
     .discarded =
         [](void *data, struct wp_presentation_feedback *wp_presentation_feedback) {
-          WaylandDisplay *const wd = get_wayland_display(data);
-          wp_presentation_feedback_add_listener(::wp_presentation_feedback(wd->presentation_, wd->surface_), &kPresentationFeedbackListener, data);
+          // TODO: remove it
+          printf("presentation.frame dropped\n");
         },
 }; // namespace flutter
 
@@ -394,7 +409,8 @@ const struct wp_presentation_listener WaylandDisplay::kPresentationListener = {
           WaylandDisplay *const wd = get_wayland_display(data);
 
           wd->presentation_clk_id_ = clk_id;
-          wp_presentation_feedback_add_listener(::wp_presentation_feedback(wd->presentation_, wd->surface_), &kPresentationFeedbackListener, data);
+
+          printf("presentation.clk_id: %u\n", clk_id);
         },
 };
 
@@ -688,13 +704,21 @@ ssize_t WaylandDisplay::vSyncHandler() {
     return -1;
   }
 
+  const uint64_t skipped_frames = (t_now_ns > last_frame_) ? (t_now_ns - last_frame_) / vblank_time_ns_ : 0;
+  printf("[%ju] vsync.ntfy %ju npts %5.2f skp %ju\n", t_now_ns, current_ns, (1. * before_next_vsync_time_ns / vblank_time_ns_) * 100, static_cast<uintmax_t>(skipped_frames));
+
   return 1;
 }
 
 const struct wl_callback_listener WaylandDisplay::kFrameListener = {.done = [](void *data, struct wl_callback *cb, uint32_t callback_data) {
   WaylandDisplay *const wd = get_wayland_display(data);
-  wd->last_frame_          = FlutterEngineGetCurrentTime();
 
+  /* check if we presentation time extension interface working */
+  if (wd->presentation_clk_id_ != UINT32_MAX) {
+    return;
+  }
+
+  wd->last_frame_ = FlutterEngineGetCurrentTime();
   wl_callback_destroy(cb);
   wl_callback_add_listener(wl_surface_frame(wd->surface_), &kFrameListener, data);
 }};
@@ -770,13 +794,18 @@ bool WaylandDisplay::Run() {
       }
 
       if (fds[0].revents & POLLIN) {
-        auto rv = vSyncHandler();
+        auto rv = readNotifyData();
 
         if (rv != 1) {
           return false;
         }
 
-        rv = readNotifyData();
+        if (presentation_clk_id_ != UINT32_MAX && presentation_ != nullptr) {
+          wp_presentation_feedback_add_listener(::wp_presentation_feedback(presentation_, surface_), &kPresentationFeedbackListener, this);
+          wl_display_dispatch_pending(display_);
+        }
+
+        rv = vSyncHandler();
 
         if (rv != 1) {
           return false;
@@ -793,6 +822,8 @@ bool WaylandDisplay::Run() {
 
       break;
     } while (true);
+
+    wl_display_dispatch_pending(display_);
   }
 
   return true;
